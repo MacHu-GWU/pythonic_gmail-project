707c8ad58ba8eeb341359617cc732de0f75b0469
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2025-09-17 23:48:38 -0400

    Update s06_list_messages_with_date_range.py

--- scripts/s06_list_messages_with_date_range.py
+++ scripts/s06_list_messages_with_date_range.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timezone
+"""
+**重要结论**
+
+- before, after 的筛选条件的时间戳精确到秒, 并且是 UTC 时间.
+- before, after 是包含边界的闭区间.
+"""
+
 import pythonic_gmail.api as pg
 from pythonic_gmail.tests.one import one
 
@@ -13,8 +19,17 @@ from rich import print as rprint
 # datetime =
 #   Wednesday, September 17, 2025 3:50:03 PM EST (timezone)
 #   Wednesday, September 17, 2025 7:50:03 PM UTC (timezone)
-lower = int(datetime(2025, 9, 17, 19, 50, 2, tzinfo=timezone.utc).timestamp())
-upper = int(datetime(2025, 9, 17, 19, 50, 4, tzinfo=timezone.utc).timestamp())
+timestamp = 1758138603
+# case 1
+# lower = timestamp - 7
+# upper = timestamp + 7
+# case 2
+lower = timestamp - 20
+upper = timestamp + 5
+# case 3
+# lower = timestamp
+# upper = timestamp + 20
+
 q = f"after:{lower} before:{upper}"
 print(f"{q = }")
 res = (
